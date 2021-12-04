157f877d7b6da6f07b34f7f3acb69e9e97d96909
Jakub Krejčík <j.krejda@seznam.cz>
2021-12-04 20:14:03 +0100

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public int sum (int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+
+    @Test
+    public void testSum () {
+        Main main = new Main();
+
+        assertEquals(10, main.sum(2,8));
+    }
+
+}
