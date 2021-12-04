5181fe3da771b719881b52183fae84f20fb1da0b
Jakub Krejčík <j.krejda@seznam.cz>
2021-12-04 20:47:43 +0100

    Added failtest

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -3,12 +3,19 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainTest {
-
     @Test
     public void testSum () {
         Main main = new Main();
 
         assertEquals(10, main.sum(2,8));
     }
+
+    @Test
+    public void testSum1 () {
+        Main main = new Main();
+
+        assertEquals(8, main.sum(2,8));
+    }
+
 
 }
